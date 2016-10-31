cdcba3ada7ec47cbf83caf42bac9760e36e64fd2
CarolinaHuelga <carolinahuelga@hotmail.com>
2016-10-31 11:05:08 -0600

    Se agreraon las clases de cada nivel
    Clase mundo
    Clase Nivel1
    Clase Nivel2
    Clase Nivel3
    Clase Nivel4
    Clase Nivel5

---
+++ Unicron-Valley/Nivel3.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Nivel3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Nivel3 extends World
+{
+
+    /**
+     * Constructor for objects of class Nivel3.
+     * 
+     */
+    public Nivel3()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
