3ab744113411e2a2a4e42bf6c02500c4a38a7f6d
CarolinaHuelga <carolinahuelga@hotmail.com>
2016-11-05 23:55:55 -0600

    Métodos a Nivel2 y a Unicornio
    Se agregaron más imágenes:
    -contador
    -vidas

--- Unicron-Valley/Nivel2.java
+++ Unicron-Valley/Nivel2.java
@@ -8,14 +8,57 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Nivel2 extends World
 {
-    private Counter vidas;
+    private Unicornio uni;
+    private Counter contLlaves;
     /**
-     * Constructor for objects of class Nivel2.
-     * 
+     * Constructor for objects of class Nivel2. 
      */
     public Nivel2()
     {    
         super(800, 600, 1); 
-        // setImage("");
+        contLlaves.setValue(0);
+        prepare();
+    }
+    public void act(){
+        uni.salta();
+    }
+    /**
+     *  @author Diana Huelga
+     *  @version 04-11-16
+     *  @param no hay parametros de entrada
+     *  @return -
+     */
+    public void modificaContadorLlaves(){
+        contLlaves.setValue(contLlaves.getValue()+10);
+        contLlaves.act();
+        switch(contLlaves.getValue()){
+            case 10: contLlaves.setImage("Llave10.png");   
+                break;
+            case 20: contLlaves.setImage("Llave20.png");   
+                break;
+            case 30: contLlaves.setImage("Llave30.png");   
+                break;
+            case 40: contLlaves.setImage("Llave40.png");   
+                break;
+            case 50: contLlaves.setImage("Llave50.png");   
+                break;
+            case 60: contLlaves.setImage("Llave60.png");   
+                break;
+            case 70:contLlaves.setImage("Llave70.png");   
+                break;
+            case 80: contLlaves.setImage("Llave80.png");   
+                break; 
+            case 90: contLlaves.setImage("Llave90.png");   
+                break;
+            case 100: contLlaves.setImage("Llave100.png");   
+                break;
+        }
+    }
+    private void prepare()
+    {
+        uni=new Unicornio();
+        addObject(uni,50,540);
+        contLlaves=new Counter();
+        addObject(contLlaves,110,30);
     }
 }
--- Unicron-Valley/Unicornio.java
+++ Unicron-Valley/Unicornio.java
@@ -14,6 +14,40 @@ public class Unicornio extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        tocaLlave();
     }    
+    /**
+     * Verifica si el unicornio tomo una llave y manda un mensaje a mundo
+     * para poder aumentar el contador y cambiar la imagen
+     * @author Diana Huelga
+     * @version 4-11-16
+     * @return -
+     * @param no hay parametros de entrada
+     */
+     public void tocaLlave(){
+        if(isTouching(Llave.class)){
+            World m=getWorld();
+            ((Nivel2)m).modificaContadorLlaves();            
+            removeTouching(Llave.class);
+        } 
+    }
+    /**
+     *Salto del unicornio (este metodo se ejecuta desde el mundo: Nivel2)
+     * @author Diana Huelga
+     * @version 4-11-16
+     * @return -
+     * @param no hay parametros de entrada
+     */
+    public void salta(){
+        if(Greenfoot.isKeyDown("up") && isTouching(Plataforma.class)){
+            for(int i=0 ; i<5 ; i++){
+                setLocation(getX(),getY()-8);
+            }
+        }else
+        if(Greenfoot.isKeyDown("up")){
+            for(int i=0 ; i<5 ; i++){
+                setLocation(getX(),getY()-3);
+            }
+        }
+    }
 }
