619e60cfb3eaad96b0e03a85617be1a03097339e
CarolinaHuelga <carolinahuelga@hotmail.com>
2016-11-23 19:38:01 -0600

    Se añaden los créditos

--- Unicron-Valley/Creditos.java
+++ Unicron-Valley/Creditos.java
@@ -8,14 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Creditos extends World
 {
-
+    private Botonmenu botMen;
     /**
      * Constructor for objects of class Creditos.
      * 
      */
     public Creditos()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    {   
         super(800, 600, 1); 
+        botMen=new Botonmenu();
+        addObject(botMen,400,570); 
     }
 }
