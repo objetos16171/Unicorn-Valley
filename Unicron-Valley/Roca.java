28727acac44f73e19232e007194b331deae81ac3
CarlosAlmendarez <b.almendarezcarlos@gmail.com>
2016-11-25 13:05:50 -0600

    Se agrego el enemigo principal del nivel 2 y se agrega la clase roca
    Se agrega el movimiento principal del enemigo principal del nivel 2 y el
    objeto que arroja en su mecanica principal

---
+++ Unicron-Valley/Roca.java
@@ -0,0 +1,64 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Roca here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Roca extends Actor
+{
+    private int vel;
+    
+    /**
+     *  Constructor que inicializa el atributo de la clase
+     *  @author Carlos Almendarez
+     *  @version 24-11-16
+     *  @param no hay parametros de entrada 
+     */
+    public Roca()
+    {
+        vel=6;
+    }
+    
+    /**
+     * Act - do whatever the Roca wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        cae();
+        elimina();
+    }   
+    
+    /**
+     *  Movimiento principal del objeto
+     *  @author Carlos Almendarez
+     *  @version 24-11-16
+     *  @param no hay parametros de entrada 
+     */
+    public void cae()
+    {
+        setLocation(getX(),getY()+vel);
+    }
+    
+    /**
+     *  Elimina la el objeto al llegar al borde o al tocar al unicornio y manda un mensaje al mundo si toca al unicornio
+     *  @author Carlos Almendarez
+     *  @version 24-11-16
+     *  @param no hay parametros de entrada 
+     */
+    public void elimina()
+    {
+        World w = getWorld();
+        
+        if(getY()>((Nivel2)w).LARGO-80 || isTouching(Unicornio.class))
+        {
+            if(isTouching(Unicornio.class))
+            {
+                ((Nivel2)w).decrementaVida();
+            }
+            ((Nivel2)w).removeObject(this);
+        }
+    }
+}
